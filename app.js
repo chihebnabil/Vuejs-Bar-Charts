0d59ced05c9701fe339bd2f7d0821a16ec219603
chihebnabil <chiheb.design@gmail.com>
2017-05-09 23:11:57 +0100

    add load data function

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 Vue.component('chart-item', {
-  props: ['height','title'],
-  template: '<li  style="height: {height} ;" title='+ {{title}} + '><div class="percent">{{height}}<span>%</span></div><div class="skill">Karate</div></li>'
+  props: ['height', 'title'],
+  template: `<li  style='height: {height} ;' title=' { title } '>
+  <div class="percent">{{height}}<span>%</span></div><div class="skill">{{title}}</div></li>`
 })
 
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
-    groceryList: [
-      { text: 'Vegetables' }
-    ]
+    data: {},
+    settings: {},
+
   },
   methods: {
 
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
+    loadData: function () {
+      var myUrl = "data.json"
+      this.$http.get(myUrl).then(function (response) {
+        this.data = response.body.data;
+        this.settings = response.body.settings;
+        console.log(this.data)
+      }, function (error) {
+        console.log(error.statusText);
+      });
     }
 
+  }
+  ,
+  mounted: function () {
+    this.loadData();
   }
 })
 
