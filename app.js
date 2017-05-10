550cf61230ad96c4815b5be695211719f31f9ea6
chihebnabil <chiheb.design@gmail.com>
2017-05-10 12:42:38 +0100

    chart works

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 Vue.component('chart-item', {
   props: ['height', 'title'],
   template: `<li  style='height: {height} ;' title=' { title } '>
- <div class="skill">{{title}}</div></li>`
+ <div class="skill"> {{ title }} </div></li>`
 })
 
 var app = new Vue({
@@ -9,16 +9,31 @@ var app = new Vue({
   data: {
     data: {},
     settings: {},
+    maxHeight: 0,
+    colors: []
 
   },
   methods: {
+    getColor: function (type) {
+      return this.colors[type];
+    },
+    getPercent: function (height) {
+      // console.log('getPercent(%o)', height);
+      return this.maxHeight > 0 ? Math.ceil(height * 100 / this.maxHeight) : 0;
+    },
+    getHeight: function (bar) {
+      // console.log('getHeight(%o)', bar.value);
+      var height = this.getPercent(bar.value);
 
+      return (height > 0 && height < 4 ? '4px' : height + '%');
+    },
     loadData: function () {
-      var myUrl = "data.json"
-      this.$http.get(myUrl).then(function (response) {
+      var url = "data.json"
+      this.$http.get(url).then(function (response) {
+
         this.data = response.body.data;
         this.settings = response.body.settings;
-        console.log(this.data)
+
       }, function (error) {
         console.log(error.statusText);
       });
@@ -26,8 +41,12 @@ var app = new Vue({
 
   }
   ,
-  mounted: function () {
+  created: function () {
     this.loadData();
+
+  },
+  mounted: function () {
+    
   }
 })
 
